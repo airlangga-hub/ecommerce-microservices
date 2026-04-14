ebfc330bca1dfe2068d42f042b34b070d11f3280
Airlangga <airlanggakrishna@gmail.com>
2026-04-14 10:15:49 +0700

    chore: Payment port injection

--- order/cmd/main.go
+++ order/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/airlangga-hub/ecommerce-microservices/order/config"
 	"github.com/airlangga-hub/ecommerce-microservices/order/internal/adapters/db"
 	"github.com/airlangga-hub/ecommerce-microservices/order/internal/adapters/grpc"
+	"github.com/airlangga-hub/ecommerce-microservices/order/internal/adapters/payment"
 	"github.com/airlangga-hub/ecommerce-microservices/order/internal/application/api"
 )
 
@@ -15,7 +16,11 @@ func main() {
 		log.Fatalf("Failed to connect to database. Error: %v", err)
 	}
 	
-	application := api.NewApplication(dbAdapter)
+	paymentAdapter, err := payment.NewAdapter(config.GetPaymentServiceUrl())
+	if err != nil {
+		log.Fatalf("Failed to initialize payment stub. Error: %v", err)
+	}
+	application := api.NewApplication(dbAdapter, paymentAdapter)
 	grcpAdapter := grpc.NewAdapter(application, int64(config.GetApplicationPort()))
 	
 	grcpAdapter.Run()
--- order/internal/application/api/api.go
+++ order/internal/application/api/api.go
@@ -6,14 +6,17 @@ import (
 )
 
 type Application struct {
-	db ports.DBPort
+	db      ports.DBPort
+	payment ports.PaymentPort
 }
 
-func NewApplication(db ports.DBPort) *Application {
+func NewApplication(db ports.DBPort, payment ports.PaymentPort) *Application {
 	return &Application{
-		db: db,
+		db:      db,
+		payment: payment,
 	}
 }
+
 func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
 	err := a.db.Save(&order)
 	if err != nil {
