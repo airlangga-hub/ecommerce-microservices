3d5036a710f7f45e54588bea2c663dcfb94cb4b6
Airlangga <airlanggakrishna@gmail.com>
2026-04-14 10:55:39 +0700

    chore: handle grpc error

--- order/internal/application/api/api.go
+++ order/internal/application/api/api.go
@@ -1,8 +1,13 @@
 package api
 
 import (
+	"strings"
+
 	"github.com/airlangga-hub/ecommerce-microservices/order/internal/application/domain"
 	"github.com/airlangga-hub/ecommerce-microservices/order/internal/ports"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Application struct {
@@ -22,11 +27,34 @@ func (a Application) PlaceOrder(order domain.Order) (domain.Order, error) {
 	if err != nil {
 		return domain.Order{}, err
 	}
-	
+
 	err = a.payment.Charge(&order)
 	if err != nil {
-		return domain.Order{}, err
+		st := status.Convert(err)
+
+		allErrors := make([]string, 0, 8)
+
+		for _, detail := range st.Details() {
+			switch t := detail.(type) {
+			case *errdetails.BadRequest:
+				for _, violation := range t.GetFieldViolations() {
+					allErrors = append(allErrors, violation.Description)
+				}
+			}
+		}
+
+		fieldErr := &errdetails.BadRequest_FieldViolation{
+			Field:       "payment",
+			Description: strings.Join(allErrors, "\n"),
+		}
+
+		badReq := &errdetails.BadRequest{}
+		badReq.FieldViolations = append(badReq.FieldViolations, fieldErr)
+
+		orderStatus := status.New(codes.InvalidArgument, "order creation failed")
+		statusWithDetails, _ := orderStatus.WithDetails(badReq)
+		return domain.Order{}, statusWithDetails.Err()
 	}
-	
+
 	return order, nil
 }
